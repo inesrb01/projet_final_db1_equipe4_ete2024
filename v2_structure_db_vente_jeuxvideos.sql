9530efc8154864761c656fcb08d3f9f00773db12
inesrb01 <inesrbah0406@gmail.com>
2024-07-25 20:12:20 -0400

    Update v2_structure_db_vente_jeuxvideos.groupe4.sql

--- v2_structure_db_vente_jeuxvideos.sql
+++ v2_structure_db_vente_jeuxvideos.sql
@@ -1,1 +1,51 @@
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3001', 'Tendulkar', 'Sachin', '514-123-4567', 'tsach@videotron.ca', 'TS0001', '15, Fleury Street', 'Montreal', 'QC', 'H2C 1S7', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3002', 'Rahul', 'Kapur', '587-777-3315', 'kapurR@yahoo.ca', 'RK0002', '19510, 102 Ave', 'Edmonton', 'AB', 'T5S 1K2', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3003', 'Ajay', 'Gandhi', '438-456-9870', 'ag@mail.com', 'AG0003', '50, Hey Ave', 'Burnaby', NULL, 'V5G 3H7', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3004', 'Fifi', 'Hill', '514-789-4561', 'fifih@hotmail.com', 'FH0004', '437, rue Boyer', 'Montreal', 'QC', 'H2J 3E6', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3005', 'Kapil', 'Dev', '418-251-4587', 'kapild@gmail.com', 'KD0005', '53, Adanac', 'Québec', 'QC', 'G1C 7B7', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3006', 'Anil', 'Brousseau', '819-564-2150', 'ab@wanadoo.com', 'AB0006', '240, rue des Nations', 'Saint-Laurent', 'QC', 'H4R 3G4', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3007', 'Anabelle', 'Dessalines', '604-683-6159', 'adessa@gmail.com', 'AS0007', '55, Carey Ave', 'Burnaby', 'BC', 'V5G 3H2', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3008', 'Georges', 'Bosse', NULL, 'georgesb@yahoo.com', 'GB0008', '35, Ellice Ave', 'Winnipeg', 'MB', 'R3B 1C6', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3009', 'Arnold', 'Marley', NULL, 'amarley@mail.com', 'AEY009', '2823, Westmorland St', 'Fredericton', 'NB', 'E3B 3L8', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3010', 'Andrew', 'Dube', '905-351-8668', 'adube@hotmail.com', 'AD0010', '984, Morning Street', 'Saskatoon', NULL, 'S7K 3R9', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3011', 'Jean-Jacques', 'Saunier', '902-835-3727', 'jjs@mail.com', 'JJS011', '22, Brownlow Ave', 'Dartmouth', 'NS', 'B3B 1T5', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3012', 'Anne', 'Reis', NULL, 'anner@gmail.com', 'AR0012', '240 Simcoe St N', 'Oshawa', 'ON', 'L1G 0C5', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3013', 'Claude', 'Bertrand', '877-696-3737', 'claudeb@gmail.com', 'CB1013', '39112, Empire Ave', 'St. John\'s', 'NL', 'A1E 1W6', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3014', 'Tendulkar', 'Sachin', NULL, 'sachint@mail.com', 'TS0014', '2 Rue des Villes', 'Québec', 'SK', 'S6V 7S4', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3015', 'Emilie', 'Tremblay', '902-367-3447', 'emiliet@gmail.com', 'ET0015', '1936 Great George St', 'Charlottetown', 'PE', 'C1A 4K7', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3016', 'Fifi', 'Hill', NULL, 'fifih@yahoo.com', 'FH0016', '1, Greater Boul', 'Montreal', NULL, 'H1L 4I4', 'Canada');
+
+INSERT INTO CLIENTS (CID, FIRSTNAME, LASTNAME, PHONE, EMAIL, CLIADDR, STREET, CITY, PROVINCE, CODE, COUNTRY)
+VALUES ('CL3017', 'Julien', 'Larouche', '866-467-4278', 'julienL@caramail.com', 'JL0017', '1130 Idylwyld Dr N', 'Saskatoon', 'SK', 'S7K 3R5', 'Canada');
 
